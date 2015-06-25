ea4430ca7021467f824cfea8bb652e3a4ad84778
programmer4004@gmail.com <Nick Moran>
2015-06-25 10:18:04 -0700

    Gives a regular suit of armor

--- src/discoarmor/DiscoArmor.java
+++ src/discoarmor/DiscoArmor.java
@@ -6,6 +6,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class DiscoArmor extends JavaPlugin {
   public static Logger log = Logger.getLogger("Minecraft");
@@ -19,6 +21,29 @@ public class DiscoArmor extends JavaPlugin {
     log.info("[DiscoArmor] Server stopping.");
   }
 
+  public void putArmorOn(Player me){
+    Player player = me;
+
+    Material[] helmetColors = new Material[16];
+    Material[] tunicColors = new Material[16];
+    Material[] leggingColors = new Material[16];
+    Material[] bootieColors = new Material[16];
+
+    helmetColors[0] = Material.LEATHER_HELMET;
+    tunicColors[0] = Material.LEATHER_CHESTPLATE;
+    leggingColors[0] = Material.LEATHER_LEGGINGS;
+    bootieColors[0] = Material.LEATHER_BOOTS;
+
+    ItemStack helmet = new ItemStack(helmetColors[0], 1);
+    ItemStack tunic = new ItemStack(tunicColors[0], 1);
+    ItemStack leggings = new ItemStack(leggingColors[0], 1);
+    ItemStack booties = new ItemStack(bootieColors[0], 1);
+    player.getInventory().setItem(39, helmet);
+    player.getInventory().setItem(38, tunic);
+    player.getInventory().setItem(37, leggings);
+    player.getInventory().setItem(36, booties);
+  }
+
   public boolean onCommand(CommandSender sender, Command command, 
                            String commandLabel, String[] args) {         
     if (commandLabel.equalsIgnoreCase("discoarmor")) {
@@ -29,6 +54,15 @@ public class DiscoArmor extends JavaPlugin {
         // ...and finish your code before this line.
         return true;
         }
+    }
+    else if (commandLabel.equalsIgnoreCase("putArmorOn")) {
+      if (sender instanceof Player){
+        Player me = (Player) sender;
+        //Start code
+        putArmorOn(me);
+        //End code
+        return true;
+      }
     }
     return false;
   }
