bda0a3cca93fe93e6a74e32430b6f9e031b30beb
programmer4004@gmail.com <Nick Moran>
2015-06-26 01:17:07 -0700

    Completed.

--- src/discoarmor/DiscoArmor.java
+++ src/discoarmor/DiscoArmor.java
@@ -41,23 +41,37 @@ public class DiscoArmor extends JavaPlugin {
 
   public void putArmorOn(Player me){
     final Player player = me;
-    final Color[] colors = new Color[16];
-    colors[0] = Color.BLUE;
-    colors[1] = Color.YELLOW;
-    colors[2] = Color.ORANGE;
-    colors[3] = Color.RED;
+    final Color[] colors = new Color[14];
+    colors[0] = Color.BLACK;
+    colors[1] = Color.BLUE;
+    colors[2] = Color.GREEN;
+    colors[3] = Color.GRAY;
+    colors[4] = Color.LIME;
+    colors[5] = Color.ORANGE;
+    colors[6] = Color.PURPLE;
+    colors[7] = Color.RED;
+    colors[8] = Color.SILVER;
+    colors[9] = Color.YELLOW;
+
       BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
         scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
-              Random x = new Random();
+              Random randomHelmet = new Random();
+              Random randomChest = new Random();
+              Random randomLegs = new Random();
+              Random randomBoots = new Random();
+              colorize(helmet, colors[randomHelmet.nextInt(9)]);
+              colorize(tunic, colors[randomChest.nextInt(9)]);
+              colorize(leggings, colors[randomLegs.nextInt(9)]);
+              colorize(booties, colors[randomBoots.nextInt(9)]);
 
-              player.sendMessage("The Number is: " + x.nextInt(16));
+              player.getInventory().setItem(39, helmet);
+              player.getInventory().setItem(38, tunic);
+              player.getInventory().setItem(37, leggings);
+              player.getInventory().setItem(36, booties);
               } 
           },0L, 20L);   
-    //player.getInventory().setItem(38, tunic);
-    //player.getInventory().setItem(37, leggings);
-    //player.getInventory().setItem(36, booties);
   }
 
   public boolean onCommand(CommandSender sender, Command command, 
@@ -66,20 +80,11 @@ public class DiscoArmor extends JavaPlugin {
       if (sender instanceof Player) { 
         Player me = (Player)sender;
         // Put your code after this line:
-
+        putArmorOn(me);
         // ...and finish your code before this line.
         return true;
         }
-    }
-    else if (commandLabel.equalsIgnoreCase("putArmorOn")) {
-      if (sender instanceof Player){
-        Player me = (Player) sender;
-        //Start code
-        putArmorOn(me);
-        //End code
-        return true;
-      }
-    }
-    return false;
   }
+    return false;
+}
 }
